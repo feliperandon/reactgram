4e3b3abc390406a1f1a7bc8eb457fafd04b2fbf0
feliperandon <feliperandon32@gmail.com>
2025-02-18 14:34:50 -0300

    get photo by id

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -84,9 +84,30 @@ const getUserPhotos = async (req, res) => {
   return res.status(200).json(photos);
 };
 
+// Get photo by id
+const getPhotoById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const photo = await Photo.findById(id);
+
+    // Check if photo exists
+    if (!photo) {
+      res.status(404).json({ errors: ["Foto não encontrada"] });
+      return;
+    }
+
+    res.status(200).json(photo);
+  } catch (error) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
+  }
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
+  getPhotoById,
 };
