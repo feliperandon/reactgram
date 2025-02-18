60e0d47743f493d7ca9d2eb889d1e98eb14cf26e
feliperandon <feliperandon32@gmail.com>
2025-02-18 14:19:44 -0300

    get user photo

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -6,6 +6,7 @@ const {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
+  getUserPhotos,
 } = require("../controllers/PhotoController");
 
 // Middlewares
@@ -25,5 +26,6 @@ router.post(
 );
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", authGuard, getUserPhotos);
 
 module.exports = router;
