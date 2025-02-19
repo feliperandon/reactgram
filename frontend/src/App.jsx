73f93dcd7697bcb8814c62ad7db0ccc4b0679ffa
feliperandon <feliperandon32@gmail.com>
2025-02-19 13:20:58 -0300

    navbar/footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,21 @@ import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
