16089951302a82939e2bdc34da8caec37e97d33b
feliperandon <feliperandon32@gmail.com>
2025-03-18 11:02:41 -0300

    form style

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -10,10 +10,15 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>ReactGram</h2>
-      <p>Cadastre-se para ver as fotos dos seus amigos.</p>
-      <form onSubmit={handleSubmit}>
+    <div className="border border-[#363636] bg-black px-6 py-8 max-w-[33%] my-8 mx-auto">
+      <h2 className="text-center text-[2.1em] mt-0 font-bold">ReactGram</h2>
+      <p className="text-center my-4 text-[#999]">
+        Cadastre-se para ver as fotos dos seus amigos.
+      </p>
+      <form
+        className="pb-6 mb-6 border-b border-[#363636]"
+        onSubmit={handleSubmit}
+      >
         <input type="text" placeholder="Nome" className="px-2.5 py-2" />
         <input type="email" placeholder="Email" className="px-2.5 py-2" />
         <input type="password" placeholder="Senha" className="px-2.5 py-2" />
@@ -22,10 +27,13 @@ const Register = () => {
           placeholder="Confirme a senha"
           className="px-2.5 py-2"
         />
-        <input type="submit" value="Cadastrar" className="px-2.5 py-2" />
+        <input type="submit" value="Cadastrar" className="px-2.5 py-2 mt-5" />
       </form>
-      <p>
-        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      <p className="text-center">
+        Já tem conta?{" "}
+        <Link to="/login" className="font-bold text-[#0094f6]">
+          Clique aqui.
+        </Link>
       </p>
     </div>
   );
