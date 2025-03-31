e939335d5e052ba37b1e4536fd468fb8f389873f
feliperandon <feliperandon32@gmail.com>
2025-03-31 12:06:46 -0300

    creating authSlice

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,5 +1,9 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="border border-[#363636] bg-black px-6 py-8 max-w-[33%] my-8 mx-auto">
+      Login
+    </div>
+  );
 };
 
 export default Login;
