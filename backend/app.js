60e022a6e793166c635cc95551e4f7ea60e58074
feliperandon <feliperandon32@gmail.com>
2025-04-09 10:53:02 -0300

    fix: port

--- backend/app.js
+++ backend/app.js
@@ -13,13 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: "http:/localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
 // Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // DB connection
 require("./config/db.js");
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API Working!");
+});
 
 // routes
 const router = require("./routes/Router.js");
