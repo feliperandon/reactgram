3a396cf7d2b82c939f3ccee6a49ec628ed3f8af6
feliperandon <feliperandon32@gmail.com>
2025-04-16 12:37:20 -0300

    authenticated user navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,16 @@ import {
   BsFillCameraFill,
 } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav className="flex justify-between items-center bg-black border-b-1 border-gray-800 p-4">
       <Link to="/">ReactGram</Link>
@@ -20,21 +29,49 @@ const Navbar = () => {
         />
       </form>
       <ul id="nav-links" className="flex items-center justify-center">
-        <li>
-          <NavLink to="/" className="mr-4 flex items-center justify-center">
-            <BsHouseDoorFill className="text-2xl" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" className="mr-4">
-            Entrar
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register" className="mr-4">
-            Cadastrar
-          </NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/" className="mr-4 flex items-center justify-center">
+                <BsHouseDoorFill className="text-2xl" />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink
+                  to={`/users/${user._id}`}
+                  className="mr-4 flex items-center justify-center"
+                >
+                  <BsFillCameraFill className="text-2xl" />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink
+                to={"/profile"}
+                className="mr-4 flex items-center justify-center"
+              >
+                <BsFillPersonFill className="text-2xl" />
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login" className="mr-4">
+                Entrar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" className="mr-4">
+                Cadastrar
+              </NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
